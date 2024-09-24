bcbdecf408cc242871d3c66b3c558966fcb57ec7
saul <saulvargascosta@hotmail.com>
2024-09-23 21:45:37 -0300

    aula

---
+++ TemDeTudo/Models/Seller.cs
@@ -0,0 +1,24 @@
+﻿using NuGet.Protocol.Core.Types;
+
+namespace TemDeTudo.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public Decimal Salary { get; set; }
+
+        public int DepatmentId { get; set; }
+
+        public Depatment Depatment { get; set; }
+
+        public List<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+    }
+
+
+}
